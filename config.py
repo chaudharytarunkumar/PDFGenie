c0c1c16bc1561b58c98d874228665b2c155f96f8
Tarun Kumar <144617663+chaudharytarunkumar@users.noreply.github.com>
2025-07-20 17:12:01 +0530

    config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-OPENAI_API_KEY = os.getenv("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")
+OPENAI_API_KEY = os.getenv("use your openai api key secrets")
